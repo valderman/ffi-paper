a38cc40a22e0eed8e6d7e470c749da943ccbd578
Anton Ekblad <anton@ekblad.cc>
2015-07-31 17:33:56 +0200

    Add product type benchmarks.

--- bench.sh
+++ bench.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # How many runs?
-TIMES=100
+TIMES=20
 
 # Common Haste args
-ARGS="--opt-whole-program --onexec perf.hs"
+ARGS="--opt-whole-program --onexec -o out.js"
 
 # Haste.Foreign args
 HFARGS="-D__USE_HASTE_FOREIGN__"
@@ -19,7 +19,7 @@ COMPILE_LOOSE="hastec $ARGS"
 perf() {
     totaltime=0
     for i in $(seq 1 $TIMES) ; do
-        sudo nice -n -20 time -o time.tmp -f %U js perf.js > /dev/null
+        sudo nice -n -20 time -o time.tmp -f %U js out.js > /dev/null
         totaltime=$(echo "$totaltime+$(cat time.tmp)" | bc)
     done
     avgtime=$(echo "scale=2; $totaltime/$TIMES" | bc)
@@ -29,18 +29,21 @@ perf() {
 compare_ffis() {
     compile="$1"
     name="$2"
+    file="$3"
     echo "=== $name ==="
-    $($compile $HFARGS)
+    $($compile $HFARGS $file)
     perf "Haste.Foreign"
     haste="$avgtime"
 
-    $($compile $FFIARGS)
+    $($compile $FFIARGS $file)
     perf "FFI"
     ratio=$(echo "scale=2; $haste/$avgtime" | bc)
     echo "Haste.Foreign/FFI avg. time ratio: $ratio\n"
 }
 
-compare_ffis "$COMPILE_TIGHT" "tight loop (no inbound)"
-compare_ffis "$COMPILE_LOOSE" "loose loop (no inbound)"
-compare_ffis "$COMPILE_TIGHT -D__MARSHAL_INBOUND__" "tight loop (w/ inbound)"
-compare_ffis "$COMPILE_LOOSE -D__MARSHAL_INBOUND__" "loose loop (w/ inbound)"
+compare_ffis "$COMPILE_TIGHT" "tight loop (no inbound)" "perf.hs"
+compare_ffis "$COMPILE_LOOSE" "loose loop (no inbound)" "perf.hs"
+compare_ffis "$COMPILE_TIGHT -D__MARSHAL_INBOUND__" "tight loop (w/ inbound)" "perf.hs"
+compare_ffis "$COMPILE_LOOSE -D__MARSHAL_INBOUND__" "loose loop (w/ inbound)" "perf.hs"
+compare_ffis "$COMPILE_TIGHT" "tight struct" "structperf.hs"
+compare_ffis "$COMPILE_LOOSE" "loose struct" "structperf.hs"
---
+++ structperf.hs
@@ -0,0 +1,66 @@
+{-# LANGUAGE OverloadedStrings, ForeignFunctionInterface, CPP #-}
+import Data.Word
+import Haste.Foreign
+import Control.Applicative
+import Data.Time.Clock
+import Foreign
+import Foreign.C.Types
+
+data MyTime = MyTime {
+    secs  :: Word,
+    usecs :: Word
+  } deriving Show
+
+instance FromAny MyTime where
+  fromAny x = MyTime <$> get x "secs" <*> get x "usecs"
+
+instance ToAny MyTime where
+  toAny (MyTime s u) = toObject [("secs", toAny s), ("usecs", toAny u)]
+
+getMyTime :: MyTime -> IO MyTime
+getMyTime =
+  ffi "function(tv){var ms = new Date().getTime();\
+                    return {secs:  ms/1000,\
+                            usecs: (ms % 1000)*1000};}"
+
+instance Storable MyTime where
+	sizeOf _ = (sizeOf (undefined :: CLong)) * 2
+	alignment _ = alignment (undefined :: CLong)
+	peek p = do
+		MyTime <$> peekElemOff (castPtr p) 0
+                       <*> peekElemOff (castPtr p) 1
+	poke p (MyTime s mus) = do
+		pokeElemOff (castPtr p) 0 s
+		pokeElemOff (castPtr p) 1 mus
+
+foreign import ccall unsafe "gettimeofday"
+   gettimeofday :: Ptr MyTime -> Ptr () -> IO CInt
+
+getTime :: MyTime -> IO MyTime
+getTime tv = with tv $ \ptval -> do
+  gettimeofday ptval nullPtr
+  peek ptval
+
+bench :: (MyTime -> IO MyTime) -> Int -> IO ()
+bench act n = go n
+  where
+    go 0   = return ()
+    go n = do
+      x <- act (MyTime 0 0)
+      x `seq` go (n-1)
+
+#ifdef __USE_HASTE_FOREIGN__
+getT = getMyTime
+#else
+getT = getTime
+#endif
+
+#ifdef __USE_TIGHT_LOOP__
+main = bench getT 10000
+#else
+main = do
+  x <- flip mapM_ [1..1000::Int] $ \_ -> do
+    x <- getT (MyTime 0 0)
+    return ()
+  return ()
+#endif
